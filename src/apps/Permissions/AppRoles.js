bda22de811020e9dc8899a8e9a7983e1c3afbfc8
Otto G <ottodevs@gmail.com>
2019-06-26 06:19:19 +0200

    Add missing proptypes

--- src/apps/Permissions/AppRoles.js
+++ src/apps/Permissions/AppRoles.js
@@ -82,8 +82,10 @@ class AppRoles extends React.PureComponent {
 class RoleRow extends React.Component {
   static propTypes = {
     onManage: PropTypes.func.isRequired,
-    role: PropTypes.shape({ bytes: PropTypes.string }).isRequired,
+    role: PropTypes.shape({ bytes: PropTypes.string, name: PropTypes.string })
+      .isRequired,
     manager: PropTypes.shape({
+      app: PropTypes.object,
       type: PropTypes.string,
       address: EthereumAddressType,
     }).isRequired,
